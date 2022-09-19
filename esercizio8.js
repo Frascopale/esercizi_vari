b3e886f05d3214954247e6c3cc1e4b415fe1bbdf
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-19 15:24:08 +0200

    esercizio8 non funziona

---
+++ esercizio8.js
@@ -0,0 +1,16 @@
+const prompt =require ('prompt-sync')();
+
+let num = 0;
+let j= 2;
+let k=0;
+
+num = prompt ("Digita il numero (deve essere un numero intero maggiore di 1): ");
+num = parseInt(num);
+
+console.log("I numeri primi sono : " )
+
+for(i=2; i<= num; i++){
+  
+}
+
+
---
+++ esercizio9.js
@@ -0,0 +1,7 @@
+const prompt =require ('prompt-sync')();
+
+let lista = ["nome", "casa", "ciao", "tre", "cane"];
+let parola= "";
+
+parola = prompt ("Digita la parola: ");
+
