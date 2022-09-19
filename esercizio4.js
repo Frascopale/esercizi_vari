088c0eb7a0910d382953148f42a123586d78b344
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-19 15:56:28 +0200

    esercizio 4 senza bonus

--- esercizio4.js
+++ esercizio4.js
@@ -2,8 +2,9 @@ const prompt =require ('prompt-sync')();
 
 let numero = prompt ("Digita un numero (intero): ");
 let somma =0;
+
 for (i=0; i<= numero; i++ ){
   somma= somma+i;
-    
 }
+
 console.log("La somma è: "+ somma);
