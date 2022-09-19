28282931f878d509de1e2209767cb4e91da51e9f
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-19 11:46:36 +0200

    Esercizio 2 completato senza bonus

--- esercizio2.js
+++ esercizio2.js
@@ -1,1 +1,6 @@
 const prompt =require ('prompt-sync')();
+
+let nome = prompt (" Scrivi il tuo nome ");
+let età = prompt (" Inserisci la tua età ");
+
+console.log(" Nome: "+ nome +" - "+ " Età: "+ età);
