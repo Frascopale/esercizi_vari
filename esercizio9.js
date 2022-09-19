1f01d69e9d9c9c63f3e30e6bc570211493b89845
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-19 15:37:41 +0200

    esercizio9 non completato

--- esercizio9.js
+++ esercizio9.js
@@ -2,6 +2,12 @@ const prompt =require ('prompt-sync')();
 
 let lista = ["nome", "casa", "ciao", "tre", "cane"];
 let parola= "";
+let i=0; 
 
 parola = prompt ("Digita la parola: ");
 
+for (i=0; i<4;i++){
+    if(parola== lista(i)){
+        console.log("La parola è presente " )
+    }
+}
