7672f0d6eda425be91f6316ee6120bef21768c79
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-23 14:43:12 +0200

    Esercisio 8 completo senza bonus

--- esercizio9.js
+++ esercizio9.js
@@ -1,13 +1,18 @@
 const prompt =require ('prompt-sync')();
 
 let lista = ["nome", "casa", "ciao", "tre", "cane"];
-let parola= "";
-let i=0; 
+let parola="";
+ 
 
 parola = prompt ("Digita la parola: ");
 
-for (i=0; i<4;i++){
-    if(parola== lista(i)){
+for (i=0; i<5;i++){
+
+    if(parola == lista[i]){
         console.log("La parola è presente " )
+    } else {
+        console.log("La parola non è presente")
     }
+
+
 }
