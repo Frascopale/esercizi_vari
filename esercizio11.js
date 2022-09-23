175f130f7105a73ba3977a3a7d14b655c52493a5
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-23 15:52:55 +0200

    Esercizio 11 non finito

--- esercizio11.js
+++ esercizio11.js
@@ -12,13 +12,38 @@ tot = prompt ("Quanti numeri vuoi nell'array? ");
 
 for (i=0; i<tot; i++){
     let numero = prompt ("Digita un numero: ");
-    lista[i] = numero;
+    lista[i] = Number(numero);
 }
 
 console.log(lista);
 
-let massimo = Math.min.apply(null,lista);
-console.log(massimo);
+let massimo = Math.max.apply(null,lista);
+console.log("Il numero massimo è " + massimo);
 
 let minimo = Math.min.apply(null,lista);
-console.log(minimo);
+console.log("Il numero massimo è " + minimo);
+
+let lungh = lista.length;
+console.log("Numero di elementi " + lungh);
+
+function media (vettore){
+    let somma=0;
+    lungh = vettore.length;
+        for(i=0 ; i< lungh ; i++){
+            somma = somma+ vettore [i];
+        }
+    return somma/lungh;
+}
+
+let med= media(lista);
+console.log("La media è: " +med);
+
+function vicino (vettore,media){
+    lungh = vettore.length;
+        for(i=0 ; i< lungh ; i++){
+            if (vettore[i]-media > 0){
+                    
+            } 
+        }
+    return somma/lungh;
+}
