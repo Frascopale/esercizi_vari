121f5fb70710b971bf10f8cc8cffe60f602c6c0a
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-26 16:30:04 +0200

    Esercizio 11

--- esercizio11.js
+++ esercizio11.js
@@ -21,7 +21,7 @@ let massimo = Math.max.apply(null,lista);
 console.log("Il numero massimo è " + massimo);
 
 let minimo = Math.min.apply(null,lista);
-console.log("Il numero massimo è " + minimo);
+console.log("Il numero minimo è " + minimo);
 
 let lungh = lista.length;
 console.log("Numero di elementi " + lungh);
@@ -39,11 +39,82 @@ let med= media(lista);
 console.log("La media è: " +med);
 
 function vicino (vettore,media){
-    lungh = vettore.length;
-        for(i=0 ; i< lungh ; i++){
-            if (vettore[i]-media > 0){
-                    
-            } 
+    nvicino = vettore[0];
+    
+        for(i= 1; i< vettore.length ; i++){
+           
+            if( Math.abs(vettore[i]-media)<=Math.abs(nvicino-media)){
+                nvicino=vettore[i];
+                
+            }
+           
         }
-    return somma/lungh;
+    return nvicino;
 }
+
+let nvic= vicino(lista,med);
+console.log("Il numero piu' vicino alla media contenuto nell'array è: " +nvic);
+
+/*specificate se il numero e' presente nell'array, oppure indicate quanto dista dai due numeri piu' vicini*/
+
+/*function vicino2 (vettore,media){
+    nvicino = vettore[0];
+    
+        for(i= 1; i< vettore.length ; i++){
+           
+            if( Math.abs(vettore[i]-media)<=Math.abs(nvicino-media)){
+                nvicino=vettore[i];
+                if(media==nvicino){
+                    console.log("Il valore della media è uguale ad uno dei numeri ed è: " + nvicino);
+                } else{
+                    let val1 = Math.abs( vettore[i]-media);
+                    console.log("La diff del valore più vicino è: " + val1);
+                    
+                    
+
+                    
+                }
+                
+            }
+           
+        }
+    return nvicino;
+}
+
+let nvicm= vicino2(lista,med);*/
+
+
+
+lista.sort(function(a, b) {
+  return a - b;
+});
+console.log(lista);
+
+function vicino2(lista,med){
+    let nvic= vicino(lista,med);
+    let j =0;
+    if (med==nvic){
+        console.log("Il valore della media è uguale ad uno dei numeri ed è: " + nvic);
+    } else if(med>nvic){
+        let val1 = Math.abs( nvic-med);
+        console.log("La diff del valore più vicino è: " + val1);
+            while(lista[j]!=nvic){
+                j++
+            }
+        let val2= Math.abs(lista[j+1]-med);
+        console.log("La diff del secondo valore più vicino è: " + val2);
+    } else {
+        let val1 = Math.abs( nvic-med);
+        console.log("La diff del valore più vicino è: " + val1);
+            while(lista[j]!=nvic){
+                j++
+            }
+        let val2= Math.abs(lista[j-1]-med);
+        console.log("La diff del secondo valore più vicino è: " + val2);
+    }
+
+    return "esercizio finito"
+}
+
+
+let nvicm= vicino2(lista,med);
