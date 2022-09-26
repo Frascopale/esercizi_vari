d7fb8e17c4a105d1b3321ff31f4814430eddf995
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-26 16:56:38 +0200

    esercizio 12 prima richiesta

---
+++ esercizio12.js
@@ -0,0 +1,5 @@
+/*Dato un numero n, calcola se quell'anno e' bisestile o meno (https://it.wikipedia.org/wiki/Anno_bisestile)
+
+fare in modo che la risposta non venga data immediatamente ma date all'utente la possibilita' di indovinare
+ritornate quanti mesi intercorrono tra oggi e l'anno inserito dall'utente (considerate primo ottobre)
+ritornate quante settimane passano tra la data indicata e oggi (cosiderate primo ottobre)*/
