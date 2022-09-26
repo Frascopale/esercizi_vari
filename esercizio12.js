d4fcd37d38d1c579dfdbbf9537be9802df1b71e5
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-26 16:58:08 +0200

    esercizion 12 prima richiesta

--- esercizio12.js
+++ esercizio12.js
@@ -3,3 +3,24 @@
 fare in modo che la risposta non venga data immediatamente ma date all'utente la possibilita' di indovinare
 ritornate quanti mesi intercorrono tra oggi e l'anno inserito dall'utente (considerate primo ottobre)
 ritornate quante settimane passano tra la data indicata e oggi (cosiderate primo ottobre)*/
+
+const prompt =require ('prompt-sync')();
+
+let anno ="";
+let risp ="";
+
+
+anno= prompt ("Inserisci un anno: ");
+risp= prompt ("Secondo te è bisestile? ");
+
+function bisestile (anno){
+    if(anno%400 ==0 || anno%4==0 && anno%100!=0){
+        console.log("Anno "+ anno+ " è bisestile")
+    } else{
+        console.log("Anno "+ anno+ " non è bisestile")
+    }
+    return "";
+}
+
+console.log(bisestile(anno));
+
